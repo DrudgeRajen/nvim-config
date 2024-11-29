c21bdb101845f8529f63b3867db22bf3dcaa4793
Rajendra Sharma <drudge.rajan@gmail.com>
2024-11-29 17:20:28 +0530

    support to search hidden file and folders

--- lua/drudge/plugins/telescope.lua
+++ lua/drudge/plugins/telescope.lua
@@ -5,7 +5,43 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
 
 	config = function()
-		require('telescope')
+		require('telescope').setup({
+			defaults = {
+				vimgrep_arguments = {
+					"rg",
+					"--follow", -- Follow symbolic links
+					"--hidden", -- Search for hidden files
+					"--no-heading", -- Don't group matches by each file
+					"--with-filename", -- Print the file path with the matched lines
+					"--line-number", -- Show line numbers
+					"--column", -- Show column numbers
+					"--smart-case", -- Smart case search
+
+					-- Exclude some patterns from search
+					"--glob=!**/.git/*",
+					"--glob=!**/.idea/*",
+					"--glob=!**/.vscode/*",
+					"--glob=!**/Cargo.lock",
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+					-- needed to exclude some files & dirs from general search
+					-- when not included or specified in .gitignore
+					find_command = {
+						"rg",
+						"--files",
+						"--hidden",
+						"--glob=!**/.git/*",
+						"--glob=!**/.idea/*",
+						"--glob=!**/.vscode/*",
+						"--glob=!**/Cargo.lock",
+					},
+				},
+			},
+		})
+
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
