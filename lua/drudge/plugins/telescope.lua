a04c31e2e39b292475a34e186ff85ba571c26eee
Rajendra Sharma <drudge.rajan@gmail.com>
2025-04-09 14:04:46 +0530

    removed unused plugin

--- lua/drudge/plugins/telescope.lua
+++ lua/drudge/plugins/telescope.lua
@@ -22,13 +22,13 @@ return {
             },
             vimgrep_arguments = {
               "rg",
-              "--follow",   -- Follow symbolic links
-              "--hidden",   -- Search for hidden files
-              "--no-heading", -- Don't group matches by each file
+              "--follow",        -- Follow symbolic links
+              "--hidden",        -- Search for hidden files
+              "--no-heading",    -- Don't group matches by each file
               "--with-filename", -- Print the file path with the matched lines
-              "--line-number", -- Show line numbers
-              "--column",   -- Show column numbers
-              "--smart-case", -- Smart case search
+              "--line-number",   -- Show line numbers
+              "--column",        -- Show column numbers
+              "--smart-case",    -- Smart case search
 
               -- Exclude some patterns from search
               "--glob=!**/.git/*",
@@ -62,10 +62,10 @@ return {
           },
           extensions = {
             fzf = {
-              fuzzy = true,              -- Enable fuzzy matching
+              fuzzy = true,                   -- Enable fuzzy matching
               override_generic_sorter = true, -- Use fzf for general sorters
-              override_file_sorter = true, -- Use fzf for file sorting
-              case_mode = 'smart_case',  -- Smart case for matching
+              override_file_sorter = true,    -- Use fzf for file sorting
+              case_mode = 'smart_case',       -- Smart case for matching
             },
           },
         })
